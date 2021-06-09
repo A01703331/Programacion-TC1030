8395cc1274010fe133e6b4e75599580e04563f7e
A01703331 <69860747+A01703331@users.noreply.github.com>
2021-06-08 20:11:37 -0500

    Batch de commits del 8 de Junio (Hasta las 8pm CT
    Bugs arreglados en algunos de los códigos, aun se deben de arreglar mas pero para mostrar progreso se subieron avances. 
    
    Implementación de clase abstracta, las clases AmbientePSO2 y AmbienteNGS se han renombrado a PSO2 y NGS respectivamente, se ha agregado la clase abstracta Ambiente que es heredada por PSO2 y NGS, el UML se ha actualizado para reflejar esto.
    
    El main paso de ser un archivo .c a un archivo .cpp ya que el resto del proyecto es en c++.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ * Jose Sebastian Pedrero Jimenez
+ * A01703331
+ * 11/Junio/2021
+*/
+
+#include "PSO2.h" 
+#include "NGS.h"
+// Clases donde se encuentran todos los objetos incluidos de todas las clases
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	Accesorios();
+};
